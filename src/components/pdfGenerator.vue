d2685c857f55e7fb4ef0e1dd5752eadbc1b23ea4
Mario Ramos Montesinos <mariodarken@gmail.com>
2018-10-17 16:54:04 +0200

    init

--- src/components/pdfGenerator.vue
+++ src/components/pdfGenerator.vue
@@ -1,6 +1,11 @@
 <template lang="html">
-  <div>
+  <div ref="printMe">
     <v-btn @click="generatePDF()">generatePDF</v-btn>
+    <v-btn @click="generatePDF2Canvas()">generatePDF2Canvas</v-btn>
+    
+    <!-- OUTPUT -->
+    <img :src="output">
+
   </div>
 </template>
  
@@ -8,6 +13,7 @@
    import jsPDF from 'jspdf';
     // eslint-disable-next-line
    import autoTable from 'jspdf-autotable';
+
    
   export default  {
     props: [],
@@ -16,6 +22,7 @@
     },
     data() {
       return {
+        output : null,
         todos:[
             {title: 'Title 1', description: 'description 1'},
             {title: 'Title 2', description: 'description 2'},
@@ -35,6 +42,20 @@
             let doc = new jsPDF();
             doc.text('Hello world!', 10, 10);
             doc.autoTable(columns, this.todos);
+            doc.save("export.pdf");
+        }, async  generatePDF2Canvas(){
+            const el = this.$refs.printMe;
+            // add option type to get the image version
+            // if not provided the promise will return 
+            // the canvas.
+            const options = {
+                type: 'dataURL'
+            }
+            this.output = await this.$html2canvas(el, options);
+            console.log(this.output);
+
+            let doc = new jsPDF();
+            doc.addImage(this.output, 'JPEG', 20, 20);
             doc.save("export.pdf");
         }
     },
