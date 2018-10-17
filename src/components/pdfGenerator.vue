c7ab165ed34b551a8c8d126a51baeab974a4e0a1
Mario Ramos Montesinos <mariodarken@gmail.com>
2018-10-17 16:05:27 +0200

    Hello world! pdf

---
+++ src/components/pdfGenerator.vue
@@ -0,0 +1,44 @@
+<template lang="html">
+  <div>
+    <v-btn @click="generatePDF()">generatePDF</v-btn>
+  </div>
+</template>
+ 
+<script lang="js">
+   import jsPDF from 'jspdf';
+   
+  export default  {
+    props: [],
+    mounted() {
+      
+    },
+    data() {
+      return {
+        todos:[
+            {title: 'Title 1', description: 'description 1'},
+            {title: 'Title 2', description: 'description 2'},
+            {title: 'Title 3', description: 'description 3'},
+        ]
+      }
+    },
+    methods: {
+        generatePDF(){
+            console.log("Generando pdf...");
+            
+            // eslint-disable-next-line
+            let columns = [
+               {title: 'Title', dataKey: "title"}, 
+               {title: 'Description', dataKey: "description"}
+            ];
+            
+            let doc = new jsPDF();
+            doc.text('Hello world!', 10, 10);
+            //doc.autoTable(columns, this.todos);
+            doc.save("export.pdf");
+        }
+    },
+    computed: {
+ 
+    }
+}
+</script>
--- src/router.js
+++ src/router.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import pdfGenerator from '@/components/pdfGenerator'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
+  routes: [{
       path: '/',
       name: 'home',
-      component: Home
+      component: pdfGenerator
     },
     {
       path: '/about',
@@ -17,7 +16,7 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import( /* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
 })
